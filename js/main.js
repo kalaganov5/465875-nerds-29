2e0e7f9a63f209684a398a4218b53bf2e16c70fa
kalaganov5 <kalaganov5@gmail.com>
2020-10-14 12:00:32 +0300

    Доработка доступности, анимации

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
+"use strict";
 /*Contact form*/
 let writeBtn = document.querySelector(".main-footer-button");
-let modalForm = document.querySelector(".modal-hidden")
+let modalForm = document.querySelector(".modal-hidden");
 let modalCloseBtn = document.querySelector(".modal-close");
 
-writeBtn.removeAttribute("href");
-writeBtn.style.cursor = "pointer";
-writeBtn.onclick = function() {
-	modalForm.classList.toggle("modal-hidden");
-}
+writeBtn.onclick = function(evt) {
+  evt.preventDefault();
+  modalForm.classList.toggle("modal-hidden");
+  setTimeout(function() {
+    modalForm.classList.add("modal-animation");
+  }, 100);
+};
 
-modalCloseBtn.onclick = function() {
-	modalForm.classList.toggle("modal-hidden");
-}
-
+modalCloseBtn.onclick = function(evt) {
+	evt.preventDefault();
+  modalForm.classList.remove("modal-animation");
+  modalForm.classList.add("modal-animation-hide");
+  setTimeout(function() {
+    modalForm.classList.toggle("modal-hidden");
+    modalForm.classList.remove("modal-animation-hide");
+  }, 100);
+};
 
 /*hide jpg map and marker*/
 let map = document.querySelector("#main-footer-map");
